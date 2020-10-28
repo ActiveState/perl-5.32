1a64ea3358393f868f90fb773ed827ab6d568269
Nathan Rijksen <n.rijksen@gmail.com>
2020-10-28 15:18:34 -0700

    Simplified and fixed up activestate.yaml (needs pending changes on state tool perl532 branch)

--- Setup.pm
+++ Setup.pm
@@ -1,6 +1,6 @@
 #!/usr/bin/perl -w
 
-package  ActiveStateSetup;
+package  Setup;
 
 use strict;
 use warnings;
@@ -8,10 +8,16 @@ use warnings;
 use Exporter 'import';
 our @EXPORT_OK = qw(create_shortcut create_file_assoc);
 
+use lib q(.);
+use File::Path qw( mkpath );
+use File::Spec::Functions qw(catfile);
+use File::Basename qw(basename);
+use Config;
+
+use Win32;
 use Win32::API;
 use Win32::Shortcut;
 use Win32::TieRegistry;
-use File::Basename qw(basename);
 
 our $VERSION           = '0.01';
 my $SHCNE_ASSOCCHANGED = 0x8_000_000;
@@ -30,9 +36,14 @@ sub update_win32_shell() {
 }
 
 sub create_shortcut {
-    my $target  = shift;
-    my $icon    = shift;
-    my $lnkPath = shift;
+    my $start_menu_path = Win32::GetFolderPath(Win32::CSIDL_STARTMENU());
+    my $start_menu_base = catfile($start_menu_path, 'ActiveState');
+
+    my $target  = $Config{perlpath};
+    my $icon    = q();
+    my $lnkPath = catfile($start_menu_base, 'Perl.lnk');
+
+    mkpath($start_menu_base);
 
     if ( -e $lnkPath ) {
         unlink $lnkPath;
@@ -52,15 +63,15 @@ sub create_shortcut {
 }
 
 sub create_file_assoc {
-    my $cmd       = shift;
-    my $assocsRef = shift;
+    my $cmd       = $Config{perlpath};
+    my $assocsRef = ['.pl', '.perl'];
 
     my $cmd_name = basename($cmd);
     my $prog_id  = "ActiveState.${cmd_name}";
 
     # file type description
     $Registry->{"CUser\\Software\\Classes\\${prog_id}\\"} = {
-        "\\" => "$cmd_name document", 
+        "\\" => "$cmd_name document",
         "shell\\" => {
             "open\\" => {
                 "command\\" => {
